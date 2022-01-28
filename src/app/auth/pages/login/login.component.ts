24207cd98809c6c7e50ceac9a3809420c415937d
Martín <magadiflo@gmail.com>
2022-01-28 11:32:22 -0500

    Tarea y resolución - Registro de usuarios

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService } from '../../services/auth.service';
-
 import  Swal  from "sweetalert2";
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import  Swal  from "sweetalert2";
+
+import { AuthService } from '../../services/auth.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -18,11 +22,20 @@ export class RegisterComponent {
 
   constructor(
     private fb: FormBuilder,
-    private router: Router) { }
+    private router: Router,
+    private authService: AuthService) { }
 
   registro(): void {
     console.log(this.miFormulario.value);
-    this.router.navigateByUrl('/dashboard');
+    const { name, email, password } = this.miFormulario.value;
+    this.authService.registro(name, email, password)
+      .subscribe(ok => {
+        if(ok === true){
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          Swal.fire('Error', ok, 'error');
+        }
+      });
   }
 
 }
